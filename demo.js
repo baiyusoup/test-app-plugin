c0a69c2e28a3171bb891b692463e997c617ca1e9
Baiyusoup <1575727840@qq.com>
2023-01-23 16:25:53 +0800

    feat: init

---
+++ demo.js
@@ -0,0 +1,62 @@
+class Demo {
+    ID = "Demo";
+    TYPE = 1;
+
+    SEARCH_URL = "";
+    search(html, jq) {
+
+    }
+
+    SECTION_URL = "https://manhua.dmzj.com/zhegeshijiangshimorc";
+    parserSectionList(html, jq) {
+      const prefix = "manhua.dmzj.com";
+      const domList = jq.parseHTML(html);
+      const list = [];
+      jq.each(domList, function (idx, el) {
+        if (el.nodeType !== 1) {
+          return;
+        }
+        const target = el.querySelector('.cartoon_online_border');
+        if (!target) {
+          return;
+        }
+        const aList = target.getElementsByTagName('a');
+        for(let i = 0; i < aList.length; i++) {
+          const a = aList.item(i);
+          list.push({
+            name: a.textContent,
+            url:  a.href.replace(a.host, prefix)
+          })
+        }
+      })
+      return list;
+    }
+
+    parserBookDetail(html, jq) {
+      const domList = jq.parseHTML(html);
+      const list = [];
+      jq.each(domList, (idx, el) => {
+        if (el.nodeType !== 1) {
+          return;
+        }
+        const target = el.querySelector('#page_select');
+        if (!target) {
+          return;
+        }
+        const options = target.getElementsByTagName('option');
+        for(let i = 0; i < options.length; i++) {
+          const option = options.item(i);
+          list.push(option.value);
+        }
+      })
+      return list;
+    }
+
+}
+
+console.log('Hello Plugin');
+if (window.registerPlugin) {
+  const p = new Demo();
+  console.log("p", p);
+  window.registerPlugin(p);
+}
