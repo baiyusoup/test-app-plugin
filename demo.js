09ec77a3d6dc9d8c85a10b68087622d93dfd3d2b
Baiyusoup <1575727840@qq.com>
2023-01-27 11:23:48 +0800

    feat: test

--- demo.js
+++ demo.js
@@ -1,66 +1,38 @@
 class Demo {
-    ID = "Demo";
-    TYPE = 1;
+  ID = "Demo";
+  TYPE = 1;
 
-    SEARCH_URL = "";
-    search(html, jq) {
+  SEARCH_URL = "";
+  search(html, jq) {
+  }
 
-    }
-
-    SECTION_URL = "https://manhua.dmzj.com/zhegeshijiangshimorc";
-    parserSectionList(html, jq) {
-      const prefix = "manhua.dmzj.com";
-      const domList = jq.parseHTML(html);
+  SECTION_URL = "https://manhua.dmzj.com/zhegeshijiangshimorc";
+  parserSectionList(html, jq) {
+      const $ = jq.load(html);
+      const domList = $('div.cartoon_online_border').find('a');
       const list = [];
-      jq.each(domList, function (idx, el) {
-        if (el.nodeType !== 1) {
-          return;
-        }
-        const target = el.querySelector('.cartoon_online_border');
-        if (!target) {
-          return;
-        }
-        const aList = target.getElementsByTagName('a');
-        for(let i = 0; i < aList.length; i++) {
-          const a = aList.item(i);
-          list.push({
-            name: a.textContent,
-            url:  a.href.replace(a.host, prefix)
+      domList.each((i, el) => {
+      list.push({
+          name: $(el).text(),
+          url:  "https://manhua.dmzj.com" + $(el).attr('href')
           })
-        }
       })
       return list;
-    }
+  }
 
-    parserBookDetail(html, jq) {
-      const domList = jq.parseHTML(html);
+  parserBookDetail(html, jq) {
+      const $ = jq.load(html);
+      const domList = $('#page_select').find('option');
       const list = [];
-      jq.each(domList, (idx, el) => {
-        if (el.nodeType !== 1) {
-          return;
-        }
-        const target = el.querySelector('#page_select');
-        if (!target) {
-          return;
-        }
-        const options = target.getElementsByTagName('option');
-        for(let i = 0; i < options.length; i++) {
-          const option = options.item(i);
-          list.push(option.value);
-        }
+      domList.each((i, el) => {
+        list.push($(el).val())
       })
       return list;
-    }
-
+  }
 }
 
 console.log('Hello Plugin');
-if (window.registerPlugin) {
-  const p = new Demo();
-  window.registerPlugin(p);
-}
-
 if (remApp) {
-  const p = new Demo();
-  remApp.register(p);
+const p = new Demo();
+remApp.register(p);
 }
